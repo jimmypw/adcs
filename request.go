60448194466d80baa1d4ce8a197769cf7333f835
Vasiliy Vasilyuk <xorcare@gmail.com>
2022-11-24 22:53:28 +0300

    Fix the method by which the error is created
    This is a more correct approach for generating such an error.

--- request.go
+++ request.go
@@ -254,7 +254,7 @@ func (wepr *WebEnrollmentPendingRequest) Submit() (WebEnrollmentResponse, error)
 	case UNAUTHORIZED:
 		return WebEnrollmentResponse{}, errors.New("Unauthorized: Access is denied")
 	case FAIL:
-		return WebEnrollmentResponse{}, fmt.Errorf("Fail: Unknown error has occurred")
+		return WebEnrollmentResponse{}, errors.New("Fail: Unknown error has occurred")
 	default:
 		// need to try and establish what went wrong here
 		panic("The request failed and i do not know why")
