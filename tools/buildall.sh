2a8d5994b95baa821aa19e98a6009c9c8edf3c7d
James Park-Watt <james@park-watt.co.uk>
2022-11-25 05:55:26 +0000

    change build script to support specific targets make build script executable

--- tools/buildall.sh
+++ tools/buildall.sh
@@ -4,7 +4,31 @@ if [ ! -d out ]; then
     mkdir out
 fi
 
-TARGETS=$(go tool dist list)
+TARGETS=("darwin/amd64"
+"darwin/arm64"
+"freebsd/386"
+"freebsd/amd64"
+"freebsd/arm"
+"freebsd/arm64"
+"linux/386"
+"linux/amd64"
+"linux/arm"
+"linux/arm64"
+"linux/loong64"
+"netbsd/386"
+"netbsd/amd64"
+"netbsd/arm"
+"netbsd/arm64"
+"openbsd/386"
+"openbsd/amd64"
+"openbsd/arm"
+"openbsd/arm64"
+"openbsd/mips64"
+"windows/386"
+"windows/amd64"
+"windows/arm"
+"windows/arm64")
+
 VERSION=$(go run cli/adcscli/main.go -v)
 
 for line in ${TARGETS}; do
